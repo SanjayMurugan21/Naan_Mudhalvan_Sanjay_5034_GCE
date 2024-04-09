06f72b5bdbe27fc015dcf44549b69c4abc96b1cb
Sanjay M <144589737+SanjayMurugan21@users.noreply.github.com>
2024-04-09 23:46:31 +0530

    Delete Naan_Mudhalvan_Sanjay_5034_GCE directory

--- Naan_Mudhalvan_Sanjay_5034_GCE/Note-sharing-web-application-django/users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Note, Profile, TODO, Note
-# Register your models here.
-
-admin.site.register(Profile)
-admin.site.register(TODO)
-admin.site.register(Note)
